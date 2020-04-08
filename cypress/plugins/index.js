c1dabd474184c54e0bb66decd3f78ba656af409c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-04-08 14:40:53 -0400

    upgrade coverage plugin to v3

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,7 +1,12 @@
 module.exports = (on, config) => {
-  on('task', require('@cypress/code-coverage/task'))
+  require('@cypress/code-coverage/task')(on, config)
+
   on(
     'file:preprocessor',
     require('@cypress/code-coverage/use-browserify-istanbul')
   )
+
+  // important - return config because code coverage plugin
+  // modifies environment variables there
+  return config
 }
